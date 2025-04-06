8d38653ef4bc5bffda6a58f80b26861fd257eb41
Hashir <hashirahmed868@gmail.com>
2025-04-06 20:52:16 +0500

    on full article page each point will be shown on new line

--- src/components/FullArticle.jsx
+++ src/components/FullArticle.jsx
@@ -17,36 +17,54 @@ function FullArticle() {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    
-    const homeElement = document.querySelector('.home');
+    const homeElement = document.querySelector(".home");
     if (homeElement) {
-      homeElement.classList.add('white-bg');
+      homeElement.classList.add("white-bg");
     }
     return () => {
       if (homeElement) {
-        homeElement.classList.remove('white-bg');
+        homeElement.classList.remove("white-bg");
       }
     };
   }, []);
 
   return (
     <>
-   {article? <div className="container full-article text-center mt-4">
-      <h1>{article.title}</h1>
-      <p className="content">
-        {article.content.split(" ").slice(0, 120).join(" ")}
-      </p>
-      <div
-        className={`col-auto full-img-cont image-container ${
-          isDark ? "bg-light text-dark" : "bg-dark text-white"
-        }`}
-      >
-        <p className="image-text">{article.keywords}</p>
-      </div>
-      <p className="content">
-        {article.content.split(" ").slice(120).join(" ")}
-      </p>
-    </div> : <ArticleList />}
+      {article ? (
+        <div className="container full-article text-center mt-4">
+          <h1>{article.title}</h1>
+          <p className="content">
+            {article.content
+              .split(" ")
+              .slice(0, 120)
+              .join(" ")
+              .split("\n")
+              .map((part, index) =>
+                index === 0 ? (
+                  part
+                ) : (
+                  <>
+                    <br />
+                    {part}
+                  </>
+                )
+              )}
+          </p>
+
+          <div
+            className={`col-auto full-img-cont image-container ${
+              isDark ? "bg-light text-dark" : "bg-dark text-white"
+            }`}
+          >
+            <p className="image-text">{article.keywords}</p>
+          </div>
+          <p className="content">
+            {article.content.split(" ").slice(120).join(" ")}
+          </p>
+        </div>
+      ) : (
+        <ArticleList />
+      )}
     </>
   );
 }
